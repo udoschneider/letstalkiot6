0ac86ecd9ff8a0facd0d086894c8cae28726a66a
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-01-24 22:44:55 +0100

    Updated Clean Script

--- 0_cleanup/clean.sh
+++ 0_cleanup/clean.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# docker system prune -a
+docker system prune -a
+docker volume rm 5_dependencytrack_dependency-track
+
 rm -vfR ~/.cache/grype
 find . -iname "node_modules" -exec rm -vfR \{} \;
 find . -iname "package-lock.json" -exec rm -vfR \{} \;
