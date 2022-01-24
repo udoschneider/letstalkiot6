31023027ba5936f5d5f32b699503ffbff477e115
Udo Schneider <Udo.Schneider@homeaddress.de>
2022-01-24 21:09:58 +0100

    Better Cleanup

--- 0_cleanup/clean.sh
+++ 0_cleanup/clean.sh
@@ -4,5 +4,5 @@
 rm -vfR ~/.cache/grype
 find . -iname "node_modules" -exec rm -vfR \{} \;
 find . -iname "package-lock.json" -exec rm -vfR \{} \;
-find . -iname "bom.xml" -exec rm -vfR \{} \;
-find . -iname "bom.json" -exec rm -vfR \{} \;
+find . -iname "*bom.xml" -exec rm -vfR \{} \;
+find . -iname "*bom.json" -exec rm -vfR \{} \;
